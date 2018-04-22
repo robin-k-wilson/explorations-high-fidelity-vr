5105d69ff59245933d81318ffd6352dbc3023022
robin-k-wilson <robin.kath.wilson@gmail.com>
2018-04-22 09:02:04 -0700

    added user data

--- wand/wand.js
+++ wand/wand.js
@@ -1,12 +1,6 @@
 (function () {
     var listOfSpawnedTrees = [];
 
-
-
-
-
-
-    
     var TRIGGER_CONTROLS = [
         Controller.Standard.LT,
         Controller.Standard.RT
@@ -23,6 +17,9 @@
             _this = this;
             _this.entityID = entityID;
             this.entityID = entityID;
+            Entity.editEntity(entityID, {
+                userData: "{ \"grabbableKey\": { \"wantsTrigger\": true }, \"wearable\": { \"joints\": { \"RightHand\": [ { \"x\": 0.0813, \"y\": 0.0452, \"z\": 0.0095 }, { \"x\": -0.3946, \"y\": -0.6604, \"z\": 0.4748, \"w\": -0.4275 } ], \"LeftHand\": [ { \"x\": -0.0881, \"y\": 0.0259, \"z\": 0.0159 }, { \"x\": 0.4427, \"y\": -0.6519, \"z\": 0.4592, \"w\": 0.4099 } ] } } }"
+            })
         },
         startEquip: function (entityID, args) {
             print("grabbed the wand!");
