4124b9c876b8758a4434ddfedec791c3e5e43328
robin-k-wilson <robin.kath.wilson@gmail.com>
2018-04-22 08:24:05 -0700

    working on wand script

---
+++ wand/wand.js
@@ -0,0 +1,30 @@
+(function () {
+    var listOfSpawnedTrees = [];
+
+    var TRIGGER_CONTROLS = [
+        Controller.Standard.LT,
+        Controller.Standard.RT
+    ];
+
+    function Wand() {
+    }
+
+    Wand.prototype = {
+        preload: function (entityID) {
+            _this = this;
+            _this.entityID = entityID;
+            this.entityID = entityID;
+        },
+        startEquip: function (entityID, args) {
+            print("grabbed the wand!");
+            hand = args[0] == "left" ? 0 : 1;
+            print(hand);
+        },
+        continueEquip: function (entityID, args) {
+            if (Controller.getValue(TRIGGER_CONTROLS[hand]) > .95) {
+                print("triggered!");
+            }
+        }
+    }
+
+})
